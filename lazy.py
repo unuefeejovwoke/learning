f1ca9c3a2e8c4cbbac439c5b403ed96bb53ff42e
unuefeejovwoke <66459477+unuefeejovwoke@users.noreply.github.com>
2020-08-05 02:40:02 -0700

    new code

--- lazy.py
+++ lazy.py
@@ -13,3 +13,33 @@ print("What is your age :")
 myAge = input()
 
 print("you will be " + str(int(myAge) +1) + " in a year")
+
+likes = 0
+if likes <5:
+    print("increased")
+    likes += 1
+   
+    likes += 1
+    print(likes)
+    
+likes = 0
+while likes < 4:
+    print("increased")
+    likes +=1
+    print(likes)
+    
+name = ""
+while name != "your name":
+    print("please type ur name")
+    name = input()
+    
+print("thank u")
+
+
+while True :
+    print("pleas ur name")
+    name = input()
+    if name == "jerry":
+        break
+print("thank u")
+    
