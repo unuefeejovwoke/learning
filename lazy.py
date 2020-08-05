b1cfaf89b18b33bb278baf9130e6872a04e1bbd4
unuefeejovwoke <66459477+unuefeejovwoke@users.noreply.github.com>
2020-08-05 02:34:11 -0700

    added new code

---
+++ lazy.py
@@ -0,0 +1,15 @@
+print("hello everyone")#greet user
+
+print("What is your name? :")#ask user for name
+
+myName=input()
+
+print("is good to see you, " +myName)
+
+print("Hey here is the lenght of your name:")
+print(len(myName))
+
+print("What is your age :")
+myAge = input()
+
+print("you will be " + str(int(myAge) +1) + " in a year")
